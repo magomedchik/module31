c3e06d78c2b1a2803e351e4b76df8c843acc3040
Ilyas Magomedov <magomedchik@yandex.ru>
2024-02-15 22:30:46 +0400

    test_master_branch

--- 02_registration_marks/main.py
+++ 02_registration_marks/main.py
@@ -1,8 +1,8 @@
-import re
+﻿import re
 
 
 
-text_numbers = 'А578ВЕ777 ОР233787 К901МН666 СТ46599 СНИ2929П777 666АМР666 Б992ТЕ766 комит 2'
+text_numbers = 'А578ВЕ777 ОР233787 К901МН666 СТ46599 СНИ2929П777 666АМР666 Б992ТЕ766 тест мастер ветки'
 
 result_personal_cars = re.findall(r'[АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]{2}\d{2,3}', text_numbers)
 result_taxi = re.findall(r'[АВЕКМНОРСТУХ]{2}\d{3}\d{2,3}', text_numbers)
